de5d9bbaec9aeb73f864c5ad124630aba656be93
mattlawiscool <mattlawiscool@gmail.com>
2017-08-10 14:36:54 -0700

    Functionality added.
    Lets try that again!

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.carousel').slick({
+        autoplay: true,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+
+    });
+});
